f69e425af8e49f5fda2d20192da785e9fef4f2d6
loup <loup.quehec@gmail.com>
2025-05-02 12:37:16 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
-import Image from "next/image";
+'use client'
+
+import { TerminalContextProvider } from "react-terminal";
+import { ReactTerminal } from "react-terminal"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const files = [
+    {
+      name: "story.txt",
+      content: `Once upon a time, in a world not so different from our own, there was a great calamity. The skies darkened, the earth trembled, and the seas roared. But amidst the chaos, a hero emerged. Armed with nothing but their wits and a mysterious artifact, they set out on a quest to save the world. Along the way, they encountered strange creatures, forged unlikely alliances, and uncovered secrets that would change everything. As they journeyed through enchanted forests and treacherous mountains, they realized that the true power to save the world lay not in the artifact, but within themselves.`
+    },
+    {
+      name: "README.md",
+      content: `# Save the World`
+    }
+  ];
+  const commands = {
+    help: (
+      <span>
+        <strong>help</strong> - Displays this help message<br />
+        <strong>clear</strong> - Clears the terminal<br />
+        <strong>cat &lt;filename&gt;</strong> - Displays the content of a file<br />
+        <strong>ls</strong> - Lists all files in the current directory<br />
+        <strong>calc &lt;EXPR&gt;</strong> - Evaluates a
+          mathematical expression (eg, <strong>4*4</strong>)<br />
+      </span>
+    ),
+    calc: async (expr: string) => {
+      if (!expr) {
+        return (
+          <span>
+            <strong>calc</strong> command requires an expression to evaluate.<br />
+            Example: <strong>calc 4*4</strong>
+          </span>
+        )
+      }
+      const response = await fetch(
+        `https://api.mathjs.org/v4/?expr=${encodeURIComponent(expr)}`
+      );
+      return await response.text();
+    },
+    ls: () => {
+      return (
+        <span>
+          {files.map((file) => (
+            <>
+            <span key={file.name}>{file.name}</span>
+            {file.name !== files[files.length - 1].name && ' '}
+            </>
+          ))}
+        </span>
+      )
+    },
+    cat: (filename: string) => {
+      const file = files.find((file) => file.name === filename);
+      if (!file) {
+        return (
+          <span>
+            <strong>{filename}</strong> not found.
+          </span>
+        )
+      }
+      return (
+        <span>
+          <strong>{filename}</strong>:<br />
+          {file.content}
+        </span>
+      )
+    },
+  };
+  const welcomeMessage = (
+    <span>
+      <strong>Time to save the world!</strong> <br />
+      <br />
+      Type &quot;help&quot; for all available commands. <br />
+      <br />
+    </span>
   );
+  const errorMessage = (
+    <span>
+      <strong>Command not found</strong> <br />
+      Type &quot;help&quot; for all available commands. <br />
+      <br />
+    </span>
+  );
+  
+  return (
+    <TerminalContextProvider>
+      <div className="h-screen">
+        <ReactTerminal
+          prompt=">"
+          commands={commands}
+          theme={'matrix'}
+          welcomeMessage={welcomeMessage}
+          errorMessage={errorMessage}
+        />
+      </div>
+    </TerminalContextProvider>
+  )
 }
